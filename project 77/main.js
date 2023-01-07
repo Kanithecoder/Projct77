7e2d28e764de1d8347de4050f8cce22bf91f46e1
Kanithecoder <92029378+Kanithecoder@users.noreply.github.com>
2023-01-07 10:52:42 +0530

    Add files via upload

---
+++ project 77/main.js
@@ -0,0 +1,16 @@
+var x = parseInt("num1").value;
+var z = parseInt("num2").value;
+
+var a = (x + z);
+
+function show()
+{
+    console.log("Console Report Project");
+    console.log("My Name is Kanishk");
+    console.log("15");
+    console.log("30");
+    console.log("45");
+    console.log("60");
+    
+
+}
